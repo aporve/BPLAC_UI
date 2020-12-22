12a5c90b42efbaa150bb34a90ebe52708a1b5100
aiswaryac895 <aiswarya.c@10xds.com>
2020-12-22 10:59:01 +0530

    bug fix , final submission page death

--- assets/js/death.js
+++ assets/js/death.js
@@ -1178,7 +1178,7 @@ function handleFormAddBeneficiary(event) {
             beneficiary["check2"] = dataBen.privacy_consent_beneficiary_2
         BeneficiaryList.push(beneficiary);
      customName=   field_BeneficiaryFirstName
-        $("#customer_Name").text(`Hi ${field_BeneficiaryFirstName}. Hang in there as we process your request. Expect an SMS from us within 1 to 2 working days on the status of your request.`);
+        $("#customer_Name").text(`Hi ${field_BeneficiaryFirstName}. Hang in there as we process your request. Expect an SMS from us within 1 to 2 WD on the status of your request.`);
         if (buttonCount == 1) {
             data1 = dataBen;
             console.log('data1', data1);
@@ -1777,7 +1777,7 @@ function handleForm(event) {
         $('#form_wrapper').hide();
         $('#death_data_privacy').hide();
         $('#payment').show();
-        $("#customer_Name").text(`Hi ${customName}. Hang in there as we process your request. Expect an SMS from us within 1 to 2 working days on the status of your request.`);
+        $("#customer_Name").text(`Hi ${customName}. Hang in there as we process your request. Expect an SMS from us within 1 to 2 WD on the status of your request.`);
         console.log('Data -> ', data)
 
     } else if ((comapareDates == false) && ((field_DOB !== '') || (field_DOID !== ''))) {
